f9892b426f8834c12d67724e8e1e839d14f8e7ac
Diego Carciente <diego.carciente@gmail.com>
2017-11-07 10:36:32 -0300

    recorriendo objeto en vuejs

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ new Vue ({
     texto : 'hola mundo',
     nombre: 'Diego Carciente',
     nota: 9,
-    peliculas: ['pelicula 1', 'pelicula 2', 'pelicula 3']
+    peliculas: ['pelicula 1', 'pelicula 2', 'pelicula 3'],
+    frutas: [
+      {name: 'pera', size: '10', price: 99 },
+      {name: 'manzana', size: '15', price: 999 },
+      {name: 'cambur', size: '34', price: 88 },
+    ]
   }
 });
